f585640802fff675965e240801d1804f914973a7
Stephen Wisner <swisner@purch.com>
2019-07-02 19:49:58 -0400

    Remove unused useCallback

--- src/hooks/useFeed.js
+++ src/hooks/useFeed.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect } from 'react'
 import { getFeed } from '../utils/api'
 import { toast } from 'react-toastify'
 
